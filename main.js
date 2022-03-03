26fc5574291773630539d0f767778ba0b13cc9e5
Andrey Babichev <klopipa@gmail.com>
2022-03-03 14:24:21 +0300

    fix style lang

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ window.addEventListener("scroll", function () {
 //add class navbar&&number
 const header = () => {
   window.onscroll = function () {
-    myFunction(), myValue();
+     myFunction(); 
+     //myValue();
   };
 
   function myFunction() {
@@ -37,24 +38,24 @@ const header = () => {
     }
   }
 
-  function myValue() {
-    var myValue1 = document.getElementById("value1");
-    var myValue2 = document.getElementById("value2");
-    var myValue3 = document.getElementById("value3");
-    var header = document.getElementById("value_target");
+  // function myValue() {
+  //   var myValue1 = document.getElementById("value1");
+  //   var myValue2 = document.getElementById("value2");
+  //   var myValue3 = document.getElementById("value3");
+  //   var header = document.getElementById("value_target");
 
-    if (header.clientWidth > 768) {
-      var sticky = header.offsetTop - header.offsetHeight;
-    } else {
-      var sticky = header.offsetTop - header.offsetHeight / 5;
-    }
+  //   if (header.clientWidth > 768) {
+  //     var sticky = header.offsetTop - header.offsetHeight;
+  //   } else {
+  //     var sticky = header.offsetTop - header.offsetHeight / 5;
+  //   }
 
-    if (window.pageYOffset > sticky) {
-      myValue1.classList.add("value-number");
-      myValue2.classList.add("value-number");
-      myValue3.classList.add("value-number");
-    }
-  }
+  //   if (window.pageYOffset > sticky) {
+  //     myValue1.classList.add("value-number");
+  //     myValue2.classList.add("value-number");
+  //     myValue3.classList.add("value-number");
+  //   }
+  // }
 };
 header();
 
